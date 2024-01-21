c46c910e2aaecaffba25ba10c6d57d74e4dcd744
sudarshan <sudarshan.mehtacs@gmail.com>
2024-01-20 19:53:02 -0800

    added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///llm.db"
 db = SQLAlchemy(app)
 
